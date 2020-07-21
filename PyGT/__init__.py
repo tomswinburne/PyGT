274b8fd9410835e57c8a373235b8dfdfaf3b11fa
tomswinburne <tomswinburne@gmail.com>
2020-07-21 10:05:03 +0200

    update

--- PyGT/__init__.py
+++ PyGT/__init__.py
@@ -47,7 +47,7 @@ The original Markov chain does not need to satisfy detailed balance.
 This package provides an efficient implementation of the graph transformation algorithm,
 accelerated via partial block matrix inversions [Swinburne20a]_ for abitrary
 discrete-time or continuous-time Markov chains [Kannan20a]_. Code is also provided
-for the calculation of first passage time statistics and phenomenological rate constants between endpoint macrostates [Wales09]_.
+for the calculation of first passage time statistics and phenomenological rate constants between endpoint macrostates [Wales09]_ [Swinburne20b]_.
 
 We also include code for two different approaches to the dimensionality reduction
 of Markov chains using the graph transformation algorithm. In the first
